98aa795f998a499f74929b4118e602d293f9500a
erinworth <eeworth@gmail.com>
2017-11-08 13:59:21 -0800

    Wiki CRUD completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
-
-  get 'welcome/about'
+  resources :wikis
 
   root 'welcome#index'
+  devise_for :users
+  get 'about' => 'welcome#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
